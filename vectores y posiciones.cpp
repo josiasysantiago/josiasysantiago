42b77d921bbf9306bef73ff7762864c2def67783
josiasysantiago <palaciosangeljosias@gmail.com>
2024-05-31 17:53:31 -0500

    Add files via upload

---
+++ vectores y posiciones.cpp
@@ -0,0 +1,21 @@
+//vectores y sus posiciones
+
+#include <iostream>
+using namespace std;
+int array[10];
+main(){
+	for(int i=0;i<10;i++){
+		cout<<"ingrese los numeros del array   ";
+		cin>>array[i];
+
+	}
+    		for(int i=0;i<10;i++){
+    cout<<"los numeros y sus posiciones son:"<<i << "es   " <<array[i] <<endl;
+	}
+
+}
+	
+	
+	
+	
+
